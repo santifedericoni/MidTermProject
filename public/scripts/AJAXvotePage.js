77cb2ed993fa9b24d239deebb21d0f514d12a0b0
Alan Marx <alanmarx@hotmail.com>
2020-04-07 18:36:49 +0000

    continued changes to vote page ajax logic

--- public/scripts/AJAXvotePage.js
+++ public/scripts/AJAXvotePage.js
@@ -1,4 +1,60 @@
 /* eslint-disable no-undef */
+const dragAndDrop = () => {
+
+  var dragSrcEl = null;
+
+  function handleDragStart(e) {
+    dragSrcEl = this;
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
+  }
+
+  function handleDragOver(e) {
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+    e.dataTransfer.dropEffect = 'move';
+    return false;
+  }
+
+  function handleDragEnter(e) {
+    this.classList.add('over');
+  }
+
+  function handleDragLeave(e) {
+    this.classList.remove('over');
+  }
+
+function handleDrop(e) {
+  if (e.stopPropagation) {
+    e.stopPropagation();
+  }
+  if (dragSrcEl != this) {
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+  }
+  return false;
+}
+  function handleDragEnd(e) {
+    [].forEach.call(cols, function (col) {
+      col.classList.remove('over');
+    });
+  }
+
+  var cols = document.querySelectorAll('#columns .column');
+  [].forEach.call(cols, function(col) {
+    col.addEventListener('dragstart', handleDragStart, false);
+    col.addEventListener('dragenter', handleDragEnter, false)
+    col.addEventListener('dragover', handleDragOver, false);
+    col.addEventListener('dragleave', handleDragLeave, false);
+    col.addEventListener('drop', handleDrop, false);
+    col.addEventListener('dragend', handleDragEnd, false);
+  });
+};
+
+
+
+
 const loadOptions = (url) => {
   $.get(url, function(data) {
     console.log(data);
@@ -32,6 +88,7 @@ $(document).ready(function() {
 
 
   loadOptions(url);
+  dragAndDrop();
 
   //  this should take all the elements in orden from the vote page and send it to the data base
   // $("form").submit(function(event) {
